6c7aef0bb29584c87111c1fc9a230c1f9a282dc9
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-07-15 22:17:05 +0530

    chapter-1 mysql

---
+++ MySql/chapter-1.sql
@@ -0,0 +1,30 @@
+ -- create database myDb;
+SHOW DATABASES;
+-- DROP DATABASE myDb;
+
+use myDb;
+
+
+create table users (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+email VARCHAR(100) UNIQUE,
+age INT,
+create_at TIMESTAMP DEFAULT current_timestamp
+);
+ -- Add columns
+alter table users add gender varchar(10);
+
+-- Modify Columns
+
+alter table users modify age smallint;
+
+-- Rename
+
+alter table users change name full_name varchar(100);
+
+ALTER TABLE users DROP COLUMN gender;
+
+DESCRIBE users;
+
+select * from users;
